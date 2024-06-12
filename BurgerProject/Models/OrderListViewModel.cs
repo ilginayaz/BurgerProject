b5f3ca3e0591d602e18cb63b0cfcad154d783a04
Burak Gonca <goncabrk@gmail.com>
2024-06-12 13:58:33 +0300

    login olan kullaniciya ait gecmiş siparisler listesi olusturuldu

---
+++ BurgerProject/Models/OrderListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BurgerProject.Data.Entities.Concrete;
+using BurgerProject.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BurgerProject.Models
+{
+    public class OrderListViewModel
+    {
+        
+
+
+
+        public int Id { get; set; } 
+        public OrderSize OrderSize { get; set; }
+        public int Piece { get; set; }
+       
+        public string MenuName { get; set; }
+        public double TotalPrice { get; set; }
+
+        // İlişkiler
+        public int AppUserId { get; set; }
+        public AppUser? AppUser { get; set; }
+        public List<Extra>? Extras { get; set; }
+        public int MenuId { get; set; }
+        public Menu? Menu { get; set; }
+
+    }
+}
