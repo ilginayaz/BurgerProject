04fd9ba4794a357de0717ce521621fbdaa7ae81b
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-06-13 16:39:11 +0300

    Admin -Siparişler ve Müşteriler Index safyaları yapıldı.

---
+++ BurgerProject/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using BurgerProject.Data.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BurgerProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class OrderController : Controller
+    {
+
+        private readonly BurgerDbContext _burgerDbContext;
+        public OrderController(BurgerDbContext burgerDbContext)
+        {
+                _burgerDbContext = burgerDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _burgerDbContext.Orders.ToListAsync());
+
+        }
+    }
+}
